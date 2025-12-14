5758f796e78a1f29b3fdf81964477ecfca5b5c59
krishnas-git-17 <krishnakothapalli.in@gmail.com>
2025-12-14 14:14:17 +0530

    build file

--- src/app/core/api.config.ts
+++ src/app/core/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-    BASE_URL: "https://localhost:7117/api",
-    // BASE_URL: "https://billbyte-be-4.onrender.com/api",
+    // BASE_URL: "https://localhost:7117/api",
+     BASE_URL: "https://billbyte-be-4.onrender.com/api",
 
 
     MENU: {
